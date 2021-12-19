50348e73b4d42db3dea6ee3c4907555f3c601320
Jared Boult <boultjare@ecs.vuw.ac.nz>
2021-12-19 21:42:35 +1300

    Completed day 13

---
+++ 13/day13-2.js
@@ -0,0 +1,85 @@
+const fs = require('fs');
+const input = fs.readFileSync('input.txt').toString().split("\n");
+const dots = new Map();
+const folds = [];
+
+initialiseDotsAndFolds(input, folds, dots);
+console.log(dots);
+console.log(folds);
+
+folds.forEach((x, i) => {
+    foldDots(x, dots);
+    console.log(`Fold ${i}: ${dots.size}`);
+});
+
+drawDots(dots);
+
+function initialiseDotsAndFolds(input, folds, dots){
+    input.forEach(line => {
+        // Ensure no empty lines are processed
+        if (!line) return;
+        // Process the points
+        if (line.includes(",")) {
+            const [x, y] = line.split(",");
+            const key = `x:${x} y:${y}`;
+            const point = {
+                'x': Number(x),
+                'y': Number(y)
+            };
+            dots.set(key, point);
+        } else {
+            // Process the folds
+            let [direction, lineNum] = line.split("=");
+                lineNum = Number(lineNum);
+                direction = direction.slice(-1);
+                const fold = {
+                    'direction': direction,
+                    'line': lineNum
+                }
+            folds.push(fold);
+        }
+    });
+}
+
+function foldDots(fold, dots) {
+    dots.forEach((v, k) => {
+        if(fold.direction === 'x') {
+            if(v.x > fold.line) {
+                v.x = v.x - ((v.x - fold.line) * 2);
+                dots.delete(k);
+                const newKey = `x:${v.x} y:${v.y}`;
+                dots.set(newKey, v);
+            }
+        } else {
+            if(v.y > fold.line) {
+                v.y = v.y - ((v.y - fold.line) * 2);
+                dots.delete(k);
+                const newKey = `x:${v.x} y:${v.y}`;
+                dots.set(newKey, v);
+            }
+        }
+    })
+}
+
+function findDimensions(dots){
+    let maxX = 0, maxY = 0;
+    const dim = [];
+    dots.forEach(d => {
+        if (d.x > maxX) maxX = d.x;
+        if (d.y > maxY) maxY = d.y;
+    })
+    return [maxX, maxY];
+}
+
+function drawDots(dots){
+    const [maxX, maxY] = findDimensions(dots);
+    for (let y = 0; y < maxY+1; y++){
+        let row = '';
+        for (let x = 0; x < maxX+1; x++){
+            const key = `x:${x} y:${y}`; 
+            if (dots.has(key)) row = `${row}#`;
+            else row = `${row}.`
+        }
+        console.log(row);
+    }
+}

5be71c8d491fd4fae5abb62d1428c0230cd0a0dd
Jared Boult <boultjare@ecs.vuw.ac.nz>
2022-01-04 23:13:44 +1300

    Solved day 12 part 2

--- 12/day12-1.js
+++ 12/day12-1.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 class Cave {
     constructor(label) {
         this.label = label;
@@ -11,15 +13,13 @@ class Cave {
     }
 }
 
-const fs = require('fs');
-const input = fs.readFileSync('input.txt').toString().split("\n");
+const input = fs.readFileSync('input.txt').toString().split('\n');
 
 const caves = new Map();
-new Set(input
-        .flatMap(e => e.split('-')))
-    .forEach(c => caves.set(c, new Cave(c)));
+new Set(input.flatMap((e) => e.split('-')))
+    .forEach((c) => caves.set(c, new Cave(c)));
 
-input.forEach(edge => {
+input.forEach((edge) => {
     const [from, to] = edge.split('-');
     const fromCave = caves.get(from);
     const toCave = caves.get(to);
@@ -27,27 +27,25 @@ input.forEach(edge => {
     toCave.adjacent.add(fromCave);
 });
 
-const graph = [...caves.values()];
 const simplePaths = [];
 const currentPath = [];
-depthFirstSearch(caves.get('start'), caves.get('end'));
 
 function depthFirstSearch(u, v) {
     if (u.visited) return;
     if (!u.big) u.visited = true;
     currentPath.push(u);
     if (u === v) {
-        simplePaths.push([...currentPath].map(c => c.toString()));
+        simplePaths.push([...currentPath].map((c) => c.toString()));
         u.visited = false;
         currentPath.pop();
         return;
     }
-    u.adjacent.forEach(a => depthFirstSearch(a, v));
+    u.adjacent.forEach((a) => depthFirstSearch(a, v));
     currentPath.pop();
     u.visited = false;
 }
 
+depthFirstSearch(caves.get('start'), caves.get('end'));
+
 console.log(simplePaths);
 console.log(simplePaths.length);
-
-
--- 12/day12-2.js
+++ 12/day12-2.js
@@ -1,10 +1,12 @@
+const fs = require('fs');
+
 class Cave {
     constructor(label) {
         this.label = label;
         this.big = label.toUpperCase() === label;
         this.adjacent = new Set();
         this.visited = false;
-        if(this.label === 'start' || this.label === 'end' || !this.big){
+        if (this.label === 'start' || this.label === 'end' || !this.big) {
             this.visitsLeft = 1;
         }
     }
@@ -14,15 +16,14 @@ class Cave {
     }
 }
 
-const fs = require('fs');
-const input = fs.readFileSync('input-test.txt').toString().split("\n");
+const input = fs.readFileSync('input.txt').toString().split('\n');
 
 const caves = new Map();
 new Set(input
-        .flatMap(e => e.split('-')))
-    .forEach(c => caves.set(c, new Cave(c)));
+    .flatMap((e) => e.split('-')))
+    .forEach((c) => caves.set(c, new Cave(c)));
 
-input.forEach(edge => {
+input.forEach((edge) => {
     const [from, to] = edge.split('-');
     const fromCave = caves.get(from);
     const toCave = caves.get(to);
@@ -30,45 +31,46 @@ input.forEach(edge => {
     toCave.adjacent.add(fromCave);
 });
 
-const graph = [...caves.values()];
-let simplePaths = [];
+const simplePaths = new Set();
 const currentPath = [];
-
-const smallCaves = graph.filter(c => !c.big && !(c.label === 'start' || c.label === 'end'));
-smallCaves.forEach(s => {
-    s.visitsLeft++;
-    depthFirstSearch(caves.get('start'), caves.get('end'));
-    graph.forEach(c => c.visited = false);
-    s.visitsLeft--;
-});
 
 function depthFirstSearch(u, v) {
     if (u.visited) return;
     if (!u.big) {
-        u.visitsLeft--;
-        if (!u.visitsLeft) u.visited = true;
-    } 
+        u.visitsLeft -= 1;
+        if (u.visitsLeft === 0) u.visited = true;
+    }
     currentPath.push(u);
     if (u === v) {
-        simplePaths.push([...currentPath].map(c => c.toString()));
-        console.log(simplePaths);
+        simplePaths.add([...currentPath]
+            .map((c) => c.toString())
+            .reduce((prev, curr) => prev + curr, ''));
         if (!u.big) {
-            u.visitsLeft++;
+            u.visitsLeft += 1;
             if (u.visitsLeft) u.visited = false;
         }
         currentPath.pop();
         return;
     }
-    u.adjacent.forEach(a => depthFirstSearch(a, v));
+    u.adjacent.forEach((a) => depthFirstSearch(a, v));
     currentPath.pop();
     if (!u.big) {
-        u.visitsLeft++;
+        u.visitsLeft += 1;
         if (u.visitsLeft) u.visited = false;
     } else {
         u.visited = false;
     }
 }
 
-simplePaths = simplePaths.flat(3);
-console.log(simplePaths);
-console.log(simplePaths.length);
+const smallCaves = Array.from(caves.values()).filter((c) => !c.big && !(c.label === 'start' || c.label === 'end'));
+smallCaves.forEach((s) => {
+    s.visitsLeft += 1;
+    depthFirstSearch(caves.get('start'), caves.get('end'));
+    smallCaves.forEach((r) => {
+        r.visitsLeft = 1;
+        r.visited = false;
+    });
+});
+
+// console.log(simplePaths);
+console.log(simplePaths.size);

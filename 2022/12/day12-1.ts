9f0779b063a28d247fc812dbfbdf624a2847f915
Jared Boult <16514321+jaredboult@users.noreply.github.com>
2022-12-12 22:12:31 +1300

    Day 12

---
+++ 2022/12/day12-1.ts
@@ -0,0 +1,118 @@
+import { readInput } from "../helperFunctions.ts";
+
+function solveProblem() {
+  const input = readInput();
+  const grid = input
+    .split("\n")
+    .map((x) =>
+      x.split("")
+        .map((letter) => new Square(letter, getElevation(letter)))
+    );
+
+  const [start, end] = findStartAndEnd(grid);
+  populateNeighbours(grid);
+  dijkstra(grid.flat(), start);
+  console.log(end?.distanceFromStart);
+}
+
+class Square {
+  label: string;
+  elevation: number;
+  neighbours: Square[];
+  distanceFromStart: number;
+
+  constructor(label: string, elevation: number) {
+    this.label = label;
+    this.elevation = elevation;
+    this.neighbours = [];
+    this.distanceFromStart = Infinity;
+  }
+}
+
+const getElevation = (str: string) => {
+  const values = "abcdefghijklmnopqrstuvwxyz".split("");
+  if (str === "S") {
+    return 1;
+  } else if (str === "E") {
+    return 26;
+  }
+  return values.indexOf(str) + 1;
+};
+
+const findStartAndEnd = (grid: Square[][]) => {
+  let start: Square | undefined = undefined;
+  let end: Square | undefined = undefined;
+  for (const row of grid) {
+    for (const square of row) {
+      if (square.label === "S") {
+        start = square;
+      } else if (square.label === "E") {
+        end = square;
+      }
+    }
+  }
+  return [start, end];
+};
+
+const populateNeighbours = (grid: Square[][]) => {
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      const currentSquare = grid[i][j];
+      const possibleNeighbours: Square[] = [];
+      if (i > 0) {
+        possibleNeighbours.push(grid[i - 1][j]);
+      }
+      if (i < grid.length - 1) {
+        possibleNeighbours.push(grid[i + 1][j]);
+      }
+      if (j > 0) {
+        possibleNeighbours.push(grid[i][j - 1]);
+      }
+      if (j < grid[i].length - 1) {
+        possibleNeighbours.push(grid[i][j + 1]);
+      }
+      for (const square of possibleNeighbours) {
+        if (square.elevation <= currentSquare.elevation + 1) {
+          currentSquare.neighbours.push(square);
+        }
+      }
+    }
+  }
+};
+
+function dijkstra(nodes: Square[], start: Square | undefined) {
+  if (start === undefined) return;
+
+  const visitedSet: Set<Square> = new Set();
+  start.distanceFromStart = 0;
+  const queue = [...nodes].sort((a, b) =>
+    a.distanceFromStart - b.distanceFromStart
+  );
+
+  while (queue.length) {
+    const v = queue.shift();
+    if (v === undefined) return;
+    visitedSet.add(v);
+
+    for (const u of v.neighbours) {
+      if (visitedSet.has(u)) continue;
+      const alternative = v.distanceFromStart + 1;
+      if (alternative < u.distanceFromStart) {
+        u.distanceFromStart = alternative;
+        queue.sort((a, b) => a.distanceFromStart - b.distanceFromStart);
+      }
+    }
+  }
+}
+
+const printGrid = (grid: Square[][]) => {
+  for (let i = 0; i < grid.length; i++) {
+    let row = "";
+    for (let j = 0; j < grid[i].length; j++) {
+      row = row.concat(String(grid[i][j].label));
+    }
+    console.log(row);
+  }
+};
+
+solveProblem();
